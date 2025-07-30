27768da028143296f230ff86407a1c0f3ec7c88c
Muhammad Haris <mhdharisfarooqui@gmail.com>
2025-07-30 17:40:17 -0400

    This course has been completed.
    The file has all the exercises related to this kaggle course. You can watch my code. I enjoyed alot!

--- Intro_to_programming.py
+++ Intro_to_programming.py
@@ -311,6 +311,173 @@ def get_doseage(weight):
 
 
 # Example usage
-print(get_doseage(22.1))
+# print(get_doseage(22.1))
+
+#Question 1
+
+def get_grade(score):
+
+    if score >=90:
+        grade = "A"
+    elif score >= 80:
+        grade = "B"
+    elif score >= 70:
+        grade = "C"
+    elif score >=60:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+
+# Example usage
+# score = 85
+
+# print(get_grade(70))
+
+# Question 2
+
+def cost_of_project(engraving, solid_gold):
+    if solid_gold == True:
+        total_cost = len(engraving) * 10 + 100
+    else:
+        total_cost = len(engraving) * 7 + 50
+    return total_cost
+
+example_usage = cost_of_project("Muhammad Haris", True)
+
+# print(cost_of_project("Muhammad Haris Farooqui", True))
+
+# Question 3
+
+def get_water_bill(num_gallons):
+    if num_gallons <= 8000:
+        bill = 5 * (num_gallons / 1000)
+    elif num_gallons <= 22000:
+        bill = 6 * (num_gallons / 1000)
+    elif num_gallons <= 30000:
+        bill = 7 * (num_gallons / 1000) 
+    else:
+        bill = 10 * (num_gallons / 1000)
+    return bill
+
+# Example usage
+# num_gallons = 15000
+
+# print(get_water_bill (3000000))
+
+# Question 4
+
+def get_phone_bill(gb):
+    if gb <= 15:
+        bill = 100
+    else:
+        bill = round(100 + ((gb - 15) * 100)) 
+    return bill
+
+# Example usage
+# gb = 20  
+
+# print(get_phone_bill(2000))
 
 
+flowers = "pink primrose,hard-leaved pocket orchid,canterbury bells,sweet pea,english marigold,tiger lily,moon orchid,bird of paradise,monkshood,globe thistle"
+# print(type(flowers))
+# print(flowers)
+
+flowers_list = ["pink primrose","hard-leaved pocket orchid","canterbury bells","sweet pea","english marigold","tiger lily","moon orchid","bird of paradise","monkshood","globe thistle"]
+
+# print(type(flowers_list))
+# print(flowers_list)
+
+# print(len(flowers_list))
+
+# print("first_value:", flowers_list[1])
+
+
+# print("6th_value:", flowers_list[6])
+
+
+# print("Last_value:", flowers_list[-1])
+
+# print("first_three_values:", flowers_list[:3], "last_two_values:", flowers_list[-2:])
+
+flowers_list.remove("monkshood")
+
+# print(flowers_list)
+
+# flowers_list.append("black_rose")
+
+# print(flowers_list)
+
+
+hardcover_sales = [139, 128, 172, 139, 191, 168, 170]
+
+# print(len(hardcover_sales))
+# print(min(hardcover_sales))
+# print(max(hardcover_sales))
+
+# print(sum(hardcover_sales[:5])/5)
+
+# Question 1
+
+menu = ['stewed meat with onions', 'bean soup', 'risotto with trout and shrimp','fish soup with cream and onion', 'gyro']
+
+menu.remove('bean soup')
+# print(menu)
+
+menu.append('roasted beet salad')
+# print(menu)
+
+
+# Question 2
+
+num_customers = [137, 147, 135, 128, 170, 174, 165, 146, 126, 159,
+                 141, 148, 132, 147, 168, 153, 170, 161, 148, 152,
+                 141, 151, 131, 149, 164, 163, 143, 143, 166, 171]
+
+avg_first_seven = sum(num_customers[:7])/7
+avg_last_seven = sum(num_customers[-7:])/7
+max_month = max(num_customers)
+min_month = min(num_customers)
+
+# print(avg_first_seven)
+# print(avg_last_seven)
+# print(max_month)
+# print(min_month)
+
+# Question 3
+
+flowers = "pink primrose,hard-leaved pocket orchid,canterbury bells,sweet pea,english marigold,tiger lily,moon orchid,bird of paradise,monkshood,globe thistle"
+
+# print(flowers.split(","))
+
+# alphabet = "A.B.C.D.E.F.G.H.I.J.K.L.M.N.O.P.Q.R.S.T.U.V.W.X.Y.Z"
+
+# print(alphabet.split("."))
+
+address = "Mr. H. Potter,The cupboard under the Stairs,4 Privet Drive,Little Whinging,Surrey"
+# print(address.split(","))
+
+alphabet = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+
+alphabet_x = [x==10 for x in alphabet]
+# print(alphabet_x)
+
+ratings = [1, 2, 3, 4, 5, 4, 5, 1]
+def percentage_liked(ratings):
+    people_liked = [x>=4 for x in ratings]
+    percentage_liked = (people_liked.count(True) / len(ratings))
+    return percentage_liked
+
+# print(percentage_liked(ratings))
+
+
+# Question 5
+
+num_users = [920344, 1043553, 1204334, 1458996, 1503323, 1593432, 1623463, 1843064, 1930992, 2001078]
+
+def percentage_growth(num_users, yrs_ago):
+    growth = ((num_users[len(num_users)-1]) - (num_users[len(num_users) - yrs_ago - 1])) / (num_users[len(num_users) - yrs_ago - 1])
+    return growth
+
+# print(percentage_growth(num_users, 10))
